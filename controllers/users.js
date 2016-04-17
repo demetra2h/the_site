c3aa5e7377843de2631fadbd8d4fb9bd88734431
Demetra Haloutsos <demetra2h@gmail.com>
2016-04-17 12:21:53 -0700

    adds routing

---
+++ controllers/users.js
@@ -0,0 +1,12 @@
+var User = require("../models/user");
+
+var index = function(req, res, next){
+};
+
+var show = function(req, res, next){
+};
+
+module.exports = {
+  index: index,
+  show:  show
+};
--- public/js/app.routes.js
+++ public/js/app.routes.js
@@ -5,4 +5,25 @@
     .module("app")
     .config(appRoutes);
 
+    appRoutes.$inject = ["$urlRouteProvider", "stateProvider"];
+
+    function appRoutes($urlRouteProvider, $stateProvider) {
+
+      $stateProvider
+        .state("about", {
+          url: "/",
+          templateUrl: "/js/layouts/about.html"
+        })
+        .state("contact", {
+          url: "/contact",
+          templateUrl: "/js/layouts/contact.html"
+        })
+        .state("projects", {
+          url: "/projects",
+          templateUrl: "/js/layouts/projects.html"
+        });
+
+      $urlRouteProvider.otherwise("/");
+    }
+
 })();
